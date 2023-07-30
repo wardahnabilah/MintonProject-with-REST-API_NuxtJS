e6ca753cc65c82d8e4194bfc1fe2f7c4091a63b9
unknown <wardahnabilah33@gmail.com>
2023-07-30 17:13:33 +0700

    Fix responsive design of Booking Berhasil Page

--- pages/booking-berhasil.vue
+++ pages/booking-berhasil.vue
@@ -7,10 +7,10 @@ import { useBookingStore } from '~/store/booking';
 <template>
     <main class="min-h-screen py-28 dark:bg-primary-dark text-primary-dark dark:text-white">
         <div class="w-10/12 max-w-[34rem] mx-auto rounded-xl p-12 bg-primary-light/60 dark:bg-white/25 text-lg shadow-2xl shadow-primary-dark dark:shadow-white/25">
-            <h2 class="mb-10 text-2xl text-center font-extrabold tracking-widest uppercase">Booking Berhasil !</h2>
+            <h2 class="max-xsm:max-w-[14rem] mx-auto mb-10 text-2xl text-center font-extrabold tracking-widest uppercase">Booking Berhasil !</h2>
             <!-- Detail Booking -->
             <div class="mb-8">
-                <p>Atas nama <span class="ml-12">: {{currentBooking.value.namaPemesan}}</span></p>
+                <p>Atas nama <span class="max-[400px]:ml-0 max-xsm:ml-6 ml-12">: {{currentBooking.value.namaPemesan}}</span></p>
                 <div class="flex items-start gap-2.5">
                     <p class="inline-block">Jadwal Booking</p>
                     <div class="md:flex">
@@ -27,7 +27,7 @@ import { useBookingStore } from '~/store/booking';
             <!-- Kode Booking -->
             <p class="mb-2">Kode booking Anda:</p>
             <div class="mb-10 py-3 text-2xl text-center border-2 border-primary-dark dark:border-white rounded-full">{{ `#${currentBooking.value.id}` }}</div>
-            <p class=" w-[22rem] mx-auto text-center">Tunjukkan kode booking ini kepada petugas pada saat datang ke lapangan</p>
+            <p class=" max-w-[22rem] mx-auto text-center">Tunjukkan kode booking ini kepada petugas pada saat datang ke lapangan</p>
             <NuxtLink to="/jadwal-booking" class="block mt-12 px-4 py-2 text-white text-lg text-center rounded-full bg-primary-red shadow-lg hover:cursor-pointer hover:bg-primary-red-dark shadow-primary-red/40">Oke</NuxtLink>
         </div>
     </main>
