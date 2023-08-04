750059284a7dbc72529805cf584957a03a02f5c1
unknown <wardahnabilah33@gmail.com>
2023-08-05 04:38:47 +0700

    Create SearchBooking component and add SearchBooking component to Header component

--- components/Header.vue
+++ components/Header.vue
@@ -1,18 +1,30 @@
 <script setup>
     import { useThemeStore } from '~/store/theme';
 
-    const isOpen = ref(false)
+    const isNavOpen = ref(false)
+    const isSearchOpen = ref(false)
     const theme = useThemeStore()
     const { currentRoute } = useRouter()
 
     // Toggle navbar
     function toggleNav() {
-        isOpen.value = !isOpen.value
+        isNavOpen.value = !isNavOpen.value
     }
 
     // Close navbar when moving to another page
     function closeNav() {
-        isOpen.value = false
+        isNavOpen.value = false
+    }
+
+    // Open search bar
+    function openSearch() {
+        isSearchOpen.value = true
+        closeNav()
+    }
+
+    // Close search bar
+    function closeSearch() {
+        isSearchOpen.value = false
     }
 
 </script>
@@ -24,13 +36,17 @@
                     <img class="w-28 hover:cursor-pointer" src="/assets/images/minton-logo.png" alt="Minton Logo">
                 </NuxtLink>
                 <svg @click="toggleNav" class="md:hidden w-7 mb-1 fill-white hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path></svg>
-                <nav :class="isOpen  ? 'max-md:top-16' : 'max-md:top-[-400%]'" class="absolute z-50 md:static left-0 duration-500 max-md:w-full bg-gradient-to-b from-primary-light from-50% to-white md:to-primary-light dark:from-header-dark dark:to-header-dark max-md:shadow-lg max-md:shadow-white/40">
+                <nav :class="isNavOpen  ? 'max-md:top-16' : 'max-md:top-[-600%]'" class="absolute z-50 md:static left-0 duration-500 max-md:w-full bg-gradient-to-b from-primary-light from-50% to-white md:to-primary-light dark:from-header-dark dark:to-header-dark max-md:shadow-lg max-md:shadow-white/40">
                     <ul class="md:flex">
                         <li class="max-md:py-4 pl-12 max-md:border-b">
                             <NuxtLink to="/" @click="closeNav" :class="currentRoute.path == '/' ? 'active' : ''" class="hover:text-accent-yellow hover:cursor-pointer">Beranda</NuxtLink>
                         </li>
                         <li class="max-md:py-4 pl-12 max-md:border-b">
                             <NuxtLink to="/jadwal-booking" @click="closeNav" :class="currentRoute.path == '/jadwal-booking' ? 'active' : ''" class="hover:text-accent-yellow hover:cursor-pointer">Jadwal Booking</NuxtLink>
+                        </li>
+                        <li @click="openSearch" class="flex gap-1 max-md:py-4 pl-12 max-md:border-b">
+                            <svg class="fill-primary-dark dark:fill-white hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path></svg>
+                            <span class="hover:text-accent-yellow hover:cursor-pointer">Cari Booking</span>
                         </li>
                         <li @click="theme.toggleTheme" class="flex gap-1 items-center max-md:py-4 pl-12 max-md:border-b">
                             <svg v-if="theme.isDark" class="w-5 mr-1 fill-primary-dark dark:fill-white hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path></svg>
@@ -44,6 +60,7 @@
                         </li>
                     </ul>
                 </nav>
+                <SearchBooking v-if="isSearchOpen" :closeSearch="closeSearch"/>
             </div>    
         </header>
 </template>
---
+++ components/SearchBooking.vue
@@ -0,0 +1,27 @@
+<script setup>
+    const { closeSearch } = defineProps(['closeSearch'])
+
+    const searchInput = ref('')
+
+
+    function searchBooking() {
+        searchInput.value = searchInput.value.replace('#','').trim()
+
+        closeSearch()
+
+        return navigateTo(`/detail-booking/${searchInput.value}`)
+    }
+</script>
+
+<template>
+    <section class="h-screen fixed inset-x-0 top-0 z-50">
+        <div class="pt-16 pb-6 md:pb-16 bg-primary-light dark:bg-header-dark text-primary-dark dark:text-white max-md:shadow-white/40">
+            <svg @click="closeSearch" class="absolute top-[1rem] md:top-[1.75rem] right-[1.5rem] md:right-[3rem] fill-primary-dark dark:fill-white hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>
+            <form @submit.prevent="searchBooking" class="w-6/12 md:w-4/12 mx-auto flex gap-3">
+                <input v-model="searchInput" class="w-full rounded-2xl px-5 py-2 text-primary-dark bg-white/60 dark:bg-accent-grey outline-none focus:outline-accent-yellow placeholder:text-primary-dark/60" type="text" placeholder="Masukkan id booking">
+                <button class="px-6 py-2 rounded-full bg-primary-red shadow-lg hover:bg-primary-red-dark shadow-primary-red/40 text-white">Cari</button>
+            </form>
+        </div>
+        <div class="min-h-screen absolute inset-x-0 top-0 bottom-0 -z-10 backdrop-blur-lg bg-primary-light/40 dark:bg-primary-dark/40"></div>
+    </section>
+</template>
